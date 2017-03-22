02854ee06515f29c7aafa095e2f5602643c6248d
Your Name <samarthmishra95@gmail.com>
2017-03-22 09:19:04 +0530

    added rotator text, fixed images, cleaned the nonsense commented code

--- js/cus.js
+++ js/cus.js
@@ -5,7 +5,7 @@
             var defaults = {
                 fadeSpeed: 500,
                 pauseSpeed: 100,
-				child:null
+        child:null
             };
              
             var options = $.extend(defaults, options);
@@ -14,19 +14,19 @@
                   var o =options;
                   var obj = $(this);                
                   var items = $(obj.children(), obj);
-				  items.each(function() {$(this).hide();})
-				  if(!o.child){var next = $(obj).children(':first');
-				  }else{var next = o.child;
-				  }
-				  $(next).fadeIn(o.fadeSpeed, function() {
-						$(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function() {
-							var next = $(this).next();
-							if (next.length == 0){
-									next = $(obj).children(':first');
-							}
-							$(obj).rotaterator({child : next, fadeSpeed : o.fadeSpeed, pauseSpeed : o.pauseSpeed});
-						})
-					});
+          items.each(function() {$(this).hide();})
+          if(!o.child){var next = $(obj).children(':first');
+          }else{var next = o.child;
+          }
+          $(next).fadeIn(o.fadeSpeed, function() {
+            $(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function() {
+              var next = $(this).next();
+              if (next.length == 0){
+                  next = $(obj).children(':first');
+              }
+              $(obj).rotaterator({child : next, fadeSpeed : o.fadeSpeed, pauseSpeed : o.pauseSpeed});
+            })
+          });
             });
         }
     });
@@ -35,51 +35,3 @@
  $(document).ready(function() {
         $('#rotate').rotaterator({fadeSpeed:500, pauseSpeed:100});
  });
-
-
-
- //Second rotate
-(function($){
-    $.fn.extend({ 
-        rotaterator: function(options) {
- 
-            var defaults = {
-                fadeSpeed: 500,
-                pauseSpeed: 100,
-                child:null
-            };
-             
-            var options = $.extend(defaults, options);
-         
-            return this.each(function() {
-                  var o =options;
-                  var obj = $(this);                
-                  var items = $(obj.children(), obj);
-                  items.each(function() {$(this).hide();})
-                  if(!o.child){var next = $(obj).children(':first');
-                  }else{var next = o.child;
-                  }
-                  $(next).fadeIn(o.fadeSpeed, function() {
-                        $(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function() {
-                            var next = $(this).next();
-                            if (next.length == 0){
-                                    next = $(obj).children(':first');
-                            }
-                            $(obj).rotaterator({child : next, fadeSpeed : o.fadeSpeed, pauseSpeed : o.pauseSpeed});
-                        })
-                    });
-            });
-        }
-    });
-})(jQuery);
-
- $(document).ready(function() {
-        $('#rotatehead').rotaterator({fadeSpeed:500, pauseSpeed:100});
- });
-
- $(function () {
-    $("#dialog").dialog({ autoOpen: false });
-    $('a').click(function () {
-        $("#dialog").dialog('open');
-    });
-});
